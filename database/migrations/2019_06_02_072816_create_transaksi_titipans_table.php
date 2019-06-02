4a595c246a86976f933992c098be05f39993af8d
Sandy Manutur Sihotang <sandysihotang12@gmail.com>
2019-06-02 14:40:30 +0700

    Ini aja dulu

--- database/migrations/2019_06_02_072816_create_transaksi_titipans_table.php
+++ database/migrations/2019_06_02_072816_create_transaksi_titipans_table.php
@@ -15,7 +15,12 @@ class CreateTransaksiTitipansTable extends Migration
     {
         Schema::create('transaksi_titipans', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('transaksi_id');
+            $table->unsignedInteger('produk_titipan_id');
+            $table->integer('jumlah');
             $table->timestamps();
+            $table->foreign('transaksi_id')->references('id')->on('transaksis');
+            $table->foreign('produk_titipan_id')->references('id')->on('barang_titipans');
         });
     }
 
